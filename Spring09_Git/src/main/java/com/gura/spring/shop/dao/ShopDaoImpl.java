a273c376df7a46af8992e865ad5fe1e18ecba580
Kimgura <ysp6336@naver.com>
2017-01-24 16:42:07 +0900

    shop 입금기능/상품목록페이지 구현

--- Spring09_Git/src/main/java/com/gura/spring/shop/dao/ShopDaoImpl.java
+++ Spring09_Git/src/main/java/com/gura/spring/shop/dao/ShopDaoImpl.java
@@ -1,5 +1,8 @@
 package com.gura.spring.shop.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,8 +15,11 @@ public class ShopDaoImpl implements ShopDao{
 	
 	@Override
 	public void deposit(String id, int money) {
-		// TODO Auto-generated method stub
-		
+		//전달 인자가 2개일때 dto / Map에 담아서 전달한다. Map에 담아도 MyBatis가 알아서 전달
+		Map<String, Object> param=new HashMap<String, Object>();
+		param.put("id", id);
+		param.put("money", money);
+		session.insert("shop.deposit", param );
 	}
 
 	@Override
--- Spring09_Git/src/main/webapp/WEB-INF/table.sql
+++ Spring09_Git/src/main/webapp/WEB-INF/table.sql
@@ -2,3 +2,5 @@ create table client_account
 (id varchar2(100) primary key,
 balance number check(balance >= 0),
 bonusPoint number check(bonusPoint >=0));
+
+insert into client_account values('gura', 0, 0);
